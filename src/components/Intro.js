ac708bf499cc4050e938a11c9ae884fca0032e72
Philip J <122313559+Bingbongjungegren@users.noreply.github.com>
2023-01-09 21:43:28 +0100

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,10 +7,10 @@ const lines = [
   { text: 'Philip', align: 'left', margin: ' 0 0 0 2rem' },
   { text: 'Jungegren', align: 'left', margin: '0 0 0 4rem' },
   { text: 'is a Digital designer', align: 'left', margin: '0' },
-  { text: 'specializing in ecommerce,', align: 'right', margin: '0 0 0 0' },
-  { text: 'focusing on creating', align: 'right', margin: '0 4rem 0 0' },
-  { text: 'great UX that', align: 'right', margin: '0' },
-  { text: 'converts', align: 'right', margin: '0 3rem 0 0' },
+  { text: 'specializing in,', align: 'right', margin: '0 0 0 0' },
+  { text: 'ecommerce, focusing on', align: 'right', margin: '0 4rem 0 0' },
+  { text: 'creating great UX', align: 'right', margin: '0' },
+  { text: 'that converts', align: 'right', margin: '0 3rem 0 0' },
 ];
 
 const StyledContainer = styled(Container)`
