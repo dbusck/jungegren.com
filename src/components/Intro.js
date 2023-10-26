6893e5367db8cf8c271abc5b78026715b07229c9
Philip J <122313559+Bingbongjungegren@users.noreply.github.com>
2023-10-26 17:20:54 +0200

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,9 +7,9 @@ const lines = [
   { text: 'Philip', align: 'left', margin: ' 0 0 0 2rem' },
   { text: 'Jungegren', align: 'left', margin: '0 0 0 4rem' },
   { text: 'is a digital designer', align: 'left', margin: '0' },
-  { text: 'specialising in e-commerce,', align: 'right', margin: '0 4rem 0 0' },
-  { text: 'creating user experiences', align: 'right', margin: '0 2rem 0 0' },
-  { text: 'that engage users', align: 'right', margin: '0' },
+  { text: 'specialising in,', align: 'right', margin: '0 3rem 0 0' },
+  { text: 'e-commerce and user experience', align: 'right', margin: '0 2rem 0 0' },
+  { text: ' design that engage users', align: 'right', margin: '0' },
   { text: 'and drive results', align: 'right', margin: '0 3rem 0 0' },
 ];
 
