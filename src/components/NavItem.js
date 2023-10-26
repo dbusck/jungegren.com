a623965620a15ae9eb37e33668b02c57b2b77336
Philip J <122313559+Bingbongjungegren@users.noreply.github.com>
2023-10-26 17:29:27 +0200

    Update NavItem.js

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const NavItem = styled(Link)`
   display: inline-block;
   border-radius: 20px;
-  padding: 0.25rem 1rem 1rem 1rem;
+  padding: 0.25rem 1rem 0.25rem 1rem;
   font-size: 1.6rem;
   border: 1px solid ${({ color }) => (color ? color : 'white')};
   color: ${({ color }) => (color ? color : 'white')};
