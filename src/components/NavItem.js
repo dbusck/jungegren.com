41e7d860c78d8f05f97c9cc96f4fa17b057cbee5
Philip J <122313559+Bingbongjungegren@users.noreply.github.com>
2023-10-28 23:31:00 +0200

    Update NavItem.js

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -6,8 +6,8 @@ const NavItem = styled(Link)`
   border-radius: 20px;
   padding: 0.25rem 2rem 0.25rem 2rem;
   font-size: 1.6rem;
-  border: 1px solid ${({ color }) => (color ? color : 'white')};
-  color: ${({ color }) => (color ? color : 'white')};
+  border: 1px solid ${({ color }) => (color ? color : 'black')};
+  color: ${({ color }) => (color ? color : 'black')};
   background: ${({ color }) => color && '#FFEDD9'};
 
   @media (${({ theme }) => theme.respondTo.desktop}) {
